03ab718b85ef0f28df2b0139653c5c6af4456532
AnnaOgorodova <ogorodova.a@yandex.ru>
2019-05-17 20:04:37 +0700

    Using DI for mem/file mode of storage

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,19 @@ namespace labs
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            switch (Configuration["Storage:Type"].ToStorageEnum())
+            {
+                case StorageEnum.MemCache:
+                    services.AddSingleton<IStorage<MyModData>, MemCache>();
+                    break;
+                case StorageEnum.FileStorage:
+                    services.AddSingleton<IStorage<MyModData>>(
+                        x => new FileStorage(Configuration["Storage:FileStorage:Filename"], int.Parse(Configuration["Storage:FileStorage:FlushPeriod"])));
+                    break;
+                default:
+                    throw new IndexOutOfRangeException($"Storage type '{Configuration["Storage:Type"]}' is unknown");
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
